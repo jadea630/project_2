edf975813bb6042368acfec8e56480010ec321ea
Howelandyu <59238150+Howelandyu@users.noreply.github.com>
2020-08-05 23:18:30 -0400

    Add files via upload

--- my_server.py
+++ my_server.py
@@ -1,37 +1,5 @@
 #Abraham Gale 2020
 #feel free to add functions to this part of the project, just make sure that the get_dns_response function works
-"""
-labels      63 octets or less
-names       255 octets or less
-TTL         pos value of signed 32 bit a number (how long to live in cache before removing from cache)
-UDP msg     512 octets or less
-
-NAME        compressed or uncompressed (variable length, must parse to see how long)
-RDATA       (variable legnth not a fixed size)
-
-So a query has 3 fields:
-    QNAME
-    QTYPE
-    QCLASS
-
-These 3 fields are your KEY, meaning you need to find a query with these matching keys. However you need to account for
-some special cases such as * in the fields QTYPE and QCLASS which means any type (aka meaning it must match just the name)
-
-Compressed names are only used for the query, when storing and sending to others it must be uncompressed
-
-Import time to check the time for a request that way you can implement TTL
-
-if Truncated bit is 1 then you need to resend as TCP because the message is too long for UDP
-
-***
-Break down each part of the query using a function so its cleaner and easier, it will probably
-be 5 functions total and 5 to build it back up
-
-Here are two good nameservers for your SBELT:      172.16.7.7     128.6.1.1
-As per the RFC it should also include two of the root name servers whose ip address can be found here https://en.wikipedia.org/wiki/Root_name_server
-Before I advised you to use the nameservers from the grep command, these are the ones I got from my grep command, but since yours may differ just use these.
-
-"""
 from resolver_backround import DnsResolver
 import threading
 import socket
@@ -40,15 +8,17 @@ import argparse
 from sys import argv
 from time import sleep
 from helper_funcs import DNSQuery
-import typing
 import copy
+import binascii
 
+import typing
 
 # b'\xac\xd9\x06N' =>
 
 
 def send_udp_message(message, address, port):
     """send_udp_message sends a message to UDP server
+
     message should be a hexadecimal encoded string
     """
 
@@ -82,12 +52,10 @@ def QueryRootNS(raw_query):
 
 	ns_server_list = [x["RDATA"][0] for x in response.answers if x["RDLENGTH"] > 0]
 	return ns_server_list
-
 # query abc.xyz.edu.com
 # 1. .com
 # 2. .edu.com
 # 3. .xyz.edu.com
-
 def QueryNextNS(curr_addr, ns_list: typing.List[bytearray], id):
 
 	next_ns_list = []
@@ -106,22 +74,11 @@ def QueryNextNS(curr_addr, ns_list: typing.List[bytearray], id):
 		query.question["QTYPE"] = 2
 		query.question["QCLASS"] = 1
 
-		
-		# yahoo.com.
-		# ns1.yahoo.com.
-		# com. not work
-		# check dig how to send .com request
- 		# let DIG send dig yahoo.com @localhost -p 8081 +trace first
-		# return a RESPONSE of ROOT SERVER
-		# then，DIG will ask your SERVER，this time he will ask .com，
-		# comparing your .com with .com from dig send, what difference they have
-		
+
 		raw_response = send_udp_message(query.to_bytes(), ns, 53)
 		response = DNSQuery(raw_response)
 		ns_server_list = [x["RDATA"][0] for x in response.answers if x["RDLENGTH"] > 0]
 		next_ns_list.extend(ns_server_list)
-		print(next_ns_list)
-
 
 
 	return next_ns_list
@@ -164,3 +121,32 @@ parser.add_argument('port', type=int, help='This is the port to connect to the r
 args = parser.parse_args(argv[1:])
 resolver = MyResolver(args.port)
 resolver.wait_for_requests()
+
+"""
+1. 
+msg0
+"what's ip of google.com"
+
+server => [root server] 8.8.8.8
+
+a. 8.8.8.8 finds the answer; return to the client
+b. if not, 
+rsp0 
+["9.9.9.9", "10.10.10.10"]
+
+2.
+msg1 
+google.com
+server => 8.8.8.8 ["9.9.9.9", "10.10.10.10"]
+
+msg2
+google.com
+
+3. 
+...
+
+n.
+=>
+client
+
+"""
